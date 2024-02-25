da4ac758fecb8bb03867ed25a64e8afe33728b9a
koreezus <49167180+koreezus@users.noreply.github.com>
2024-02-25 14:57:27 -0500

    silly comment

--- src/test/java/ParaBank/TestCases/StandaloneTest.java
+++ src/test/java/ParaBank/TestCases/StandaloneTest.java
@@ -2,6 +2,6 @@ package ParaBank.TestCases;
 
 public class StandaloneTest {
     public static void main(String[] args) {
-
+        //standalone test for username: testaccount password: testingpassword
     }
 }
